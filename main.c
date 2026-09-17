f86a150516270b8897bc7dea6239a35e2186eae7
Syed Alkama <syedalkama2419@gmail.com>
2026-09-17 15:50:04 +0000

    Board can now draw on the terminal with all pieces

--- main.c
+++ main.c
@@ -3,6 +3,19 @@
 #include <stdlib.h>
 #include <string.h>
 
+/* These represnts each bit of a byte, The last 2  represents black and white
+    and the first 6 represnts each pieces*/
+
+#define KING 128
+#define QUEEN 64
+#define BISHOP 32
+#define KNIGHT 16
+#define ROOK 8
+#define PAWN 4
+
+#define BLACK 2
+#define WHITE 1
+
 /* In this program, board means 128 size array, game means the 8x8 chess board*/
 uint8_t board[128];
 /* Structs for the game*/
@@ -15,22 +28,47 @@ struct gameState GAMESTATE;
 void printBoard()
 {
     printf("\x1b[7m  A B C D E F G H  \x1b[0m\n");
-    char s[80];
-    if (GAMESTATE.player == 1)
+    for (int i = 0; i < 8; i++)
     {
-        for (int i = 1; i <= 8; i++)
+        printf("\x1b[7m%d\x1b[0m ", i + 1);
+        for (int j = 0; j < 8; j++)
         {
-            snprintf(s, 80, "\x1b[7m%d\x1b[0m _ _ _ _ _ _ _ _ \x1b[7m%d\x1b[0m\n", i, i);
-            printf("%s", s);
+            int piece = board[(16 * i) + j] & 0b11111100;
+            int player = board[(16 * i) + j] & ~(piece);
+            char toPrint;
+            switch (piece)
+            {
+            case PAWN:
+                toPrint = 'p';
+                break;
+            case ROOK:
+                toPrint = 'r';
+                break;
+            case KNIGHT:
+                toPrint = 'n';
+                break;
+            case BISHOP:
+                toPrint = 'b';
+                break;
+            case QUEEN:
+                toPrint = 'q';
+                break;
+            case KING:
+                toPrint = 'k';
+                break;
+            default:
+                toPrint = '_';
+                printf("%c ", toPrint);
+                continue;
+            }
+            if (player == WHITE)
+            {
+                toPrint -= 32;
+            }
+            printf("%c ", toPrint);
         }
-    }
-    else
-    {
-        for (int i = 8; i >= 1; i--)
-        {
-            snprintf(s, 80, "\x1b[7m%d\x1b[0m _ _ _ _ _ _ _ _ \x1b[7m%d\x1b[0m\n", i, i);
-            printf("%s", s);
-        }
+
+        printf("\x1b[7m%d\x1b[0m\n", i + 1);
     }
     printf("\x1b[7m  A B C D E F G H  \x1b[0m\n");
 }
@@ -69,8 +107,37 @@ int boardToGame(uint8_t boardIndex)
     return (boardIndex + (boardIndex & 7)) >> 1;
 }
 
+/* Initializng the Game Board */
+void initBoard()
+{
+    int backpieces[8] = {ROOK, KNIGHT, BISHOP, QUEEN, KING, BISHOP, KNIGHT, ROOK};
+    int i = 0;
+    while (i <= 119)
+    {
+        int player = (i < 64) ? WHITE : BLACK;
+        if (i / 16 == 0 || i / 16 == 7)
+        {
+            board[i] = backpieces[i % 16] | player;
+        }
+        else if (i / 16 == 1 || i / 16 == 6)
+        {
+            board[i] = PAWN | player;
+        }
+        else
+        {
+            board[i] = 0;
+        }
+        if ((i + 1) % 8 == 0)
+        {
+            i += 8;
+        }
+        i++;
+    }
+}
+
 int main()
 {
+    initBoard();
     GAMESTATE.player = 2;
     printBoard();
     return 0;
