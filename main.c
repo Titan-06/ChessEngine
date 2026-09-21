b27810cfc7c98fd4ca5096e3bb2c77d9e25a075f
Syed Alkama <syedalkama2419@gmail.com>
2026-09-21 13:52:00 +0000

    Implmeneted the Check pathClear function and a proper game loop

--- main.c
+++ main.c
@@ -25,6 +25,7 @@ struct gameState
     uint8_t player; /* 1 for white and 2 for black */
     int whiteScore;
     int blackScore;
+    int moves;
 };
 struct gameState GAMESTATE;
 /* To print the board*/
@@ -135,71 +136,166 @@ uint8_t signToPiece(char sign)
 {
     switch (sign)
     {
-    case 'N':
+    case 'n':
         return KNIGHT;
-    case 'K':
+    case 'k':
         return KING;
-    case 'Q':
+    case 'q':
         return QUEEN;
-    case 'B':
+    case 'b':
         return BISHOP;
-    case 'R':
+    case 'r':
         return ROOK;
     }
 }
-int destSquare(int *piece, char *move)
+int translateNotation(char *move, int *src, int *dest) // Returns 1 normally, 0 if some error and piece value if promotion
 {
     int len = strlen(move);
     char file;
     char rank;
-    switch (len)
+
+    // Square of the piece
+    file = move[0] - 97;
+    rank = move[1] - 49;
+    *src = rankFileToBoard(rank, file);
+
+    // Square to move to
+    file = move[2] - 97;
+    rank = move[3] - 49;
+    *dest = rankFileToBoard(rank, file);
+
+    printf("Src: %d Des: %d Next Player: %d\n", *src, *dest, GAMESTATE.player);
+
+    if (len == 4)
     {
-    case 2:
-        file = move[0] - 97;
-        rank = move[1] - 49;
-        *piece = PAWN;
-        return rankFileToBoard(rank, file);
-    case 3:
-        *piece = signToPiece(move[0]);
-        file = move[1] - 97;
-        rank = move[2] - 49;
-        return rankFileToBoard(rank, file);
-    default:
-        break;
+        return 1;
+    }
+    else if (len == 5)
+    {
+        return signToPiece(move[4]);
+    }
+    else
+    {
+        return 0;
     }
 }
 
 /* Logic of the chess game */
+int pathClear(int src, int dest)
+{
+    int distance = dest - src;
+    // Get the direction
+    int dir = (distance < 0) ? -1 : 1;
+    // Get the magnitude of the steps
+    // 15 and 17 for the bishop
+    int walk;
+    if (distance % 15 == 0)
+    {
+        walk = dir * 15;
+    }
+    else if (distance % 16 == 0)
+    {
+        walk = dir * 16;
+    }
+    else if (distance % 17 == 0)
+    {
+        walk = dir * 17;
+    }
+    else
+    {
+        walk = dir;
+    }
+    // Walk in that dir and mag , return 0 if anything is encounterred
+    src += walk;
+    while (src != dest)
+    {
+        if (board[src] != 0)
+        {
+            return 0;
+        }
+        src += walk;
+    }
+    return 1;
+}
+
+int legalMove(int srcSquare, int desSquare) // Return 1 if it is , Return 0 if its not
+{
+    if ((desSquare & 0x88))
+    {
+        printf("NOT inBOUNDS\n");
+        return 0;
+    }
+
+    int distance = desSquare - srcSquare;
+    int piece = board[srcSquare] & 0b11111100;
+    int playerSrc = board[srcSquare] & 0b00000011;
+
+    // To not let the player move other piece or capture its own
+    if (!(board[srcSquare] & GAMESTATE.player) || board[desSquare] & GAMESTATE.player)
+    {
+        return 0;
+    }
+
+    if (playerSrc == WHITE && distance < 0 && board[srcSquare] & PAWN)
+    {
+        printf("%d\n", board[srcSquare]);
+        return 0;
+    }
+    else if (playerSrc == BLACK && distance > 0 && board[srcSquare] & PAWN)
+    {
+        printf("%d\n", board[srcSquare]);
+        return 0;
+    }
+
+    if (lookup[distance + 119] & piece)
+    {
+        if (piece == QUEEN || piece == ROOK || piece == BISHOP)
+        {
+            if (pathClear(srcSquare, desSquare))
+            {
+                return 1;
+            }
+            return 0;
+        }
+        else
+        {
+            return 1;
+        }
+    }
+    else
+    {
+        printf("NOT LOOKEDUP\n");
+        return 0;
+    }
+}
+
 void movePiece(char *move)
 {
-    int i = 119;
-    int piece;
-    int dest = destSquare(&piece, move);
-    printf("%d\n", dest);
-    while (i >= 0)
+    int srcSquare;
+    int desSquare;
+    if (!translateNotation(move, &srcSquare, &desSquare))
     {
-        int dist = dest - i;
-        printf("%d\n", dist);
-        if ((lookup[dist + 119] & piece))
-        {
-            board[dest] = board[i];
-            board[i] = 0;
-            break;
-        }
-
-        if (i % 8 == 0)
-        {
-            i -= 8;
-        }
-        i--;
+        printf("Couldn't Translate\n");
+        return;
     }
+    if (!legalMove(srcSquare, desSquare))
+    {
+        printf("NOT LEGAL\n");
+        return;
+    }
+    board[desSquare] = board[srcSquare];
+    board[srcSquare] = 0;
+
+    GAMESTATE.player = (GAMESTATE.player == 2) ? 1 : 2;
+    GAMESTATE.moves++;
 }
 /* Initializng the Game Board */
 void initGameState()
 {
-    GAMESTATE.player = 2;
+    GAMESTATE.player = 1;
     GAMESTATE.blackScore = 0;
     GAMESTATE.whiteScore = 0;
+    GAMESTATE.moves = 0;
 }
 void initBoard()
 {
@@ -231,25 +327,29 @@ void initLookup()
 {
     for (int i = 0; i < 240; i++)
     {
+        int dir_des = i - 119;
         lookup[i] = 0;
-        if (i == 33 || i - 119 == -33 || i == 31 || i - 119 == -31)
+        if (dir_des == 33 || dir_des == -33 || dir_des == 31 || dir_des == -31)
         {
             lookup[i] |= KNIGHT;
         }
-        if (i % 16 == 0)
+        if ((dir_des % 16 == 0) || (dir_des / 8 == 0))
         {
             lookup[i] |= ROOK;
             lookup[i] |= QUEEN;
         }
-        if ((i - 1) % 16 == 0 || (i + 1) % 16 == 0)
+        if ((dir_des) % 15 == 0 || (dir_des) % 17 == 0)
         {
             lookup[i] |= BISHOP;
             lookup[i] |= QUEEN;
         }
-        if (i == 16 || i - 119 == -16 || i == 17 || i == 15 || i - 119 == -17 || i - 119 == -15)
+        if (dir_des == 16 || dir_des == -16 || dir_des == 17 || dir_des == 15 || dir_des == -17 || dir_des == -15 || dir_des == 1 || dir_des == -1)
+        {
+            lookup[i] |= KING;
+        }
+        if (dir_des == 16 || dir_des == -16)
         {
             lookup[i] |= PAWN;
-            lookup[i] |= KING;
         }
     }
 }
@@ -257,14 +357,17 @@ void initLookup()
 int main()
 {
     char *s;
+    initGameState();
     initBoard();
     initLookup();
-    printBoard();
-    printf("AI: NO MOVEEEE\n");
-    printf("YOUR MOVE:");
-    scanf("%s", s);
-    movePiece(s);
-    printBoard();
-    printf("%s\n", s);
+
+    /* Make a Loop here to move both */
+    while (1)
+    {
+        printBoard();
+        printf("%d MOVE:", GAMESTATE.player);
+        scanf("%s", s);
+        movePiece(s);
+    }
     return 0;
 }
